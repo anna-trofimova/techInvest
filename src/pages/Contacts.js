fef9977e73d9614f50bca4847c99804efcf54f74
Anna Trogimova <cobrana1000@gmail.com>
2019-10-15 16:42:25 +0300

    contacts page

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -4,6 +4,20 @@ export default function Contacts() {
   return (
     <div>
       <h1>Контакты</h1>
+      <article>
+        <h5>Адрес:</h5>
+        <address>
+          <p>г. Москва </p>
+          <p>107014, Большая Остроумовская, д.12</p>
+        </address>
+      </article>
+      <article>
+        <h5>Телефон:</h5>
+        <p>+7(495) 585-07-59</p>
+        <p>info@veglas-e.ru</p>
+      </article>
+
+
     </div>
   )
 }
