614d78eb30547d4f59fbffd0a62291ac8d826056
Anna Trogimova <cobrana1000@gmail.com>
2019-10-22 00:21:05 +0300

    firebase, ugly but works copassible in uslugi

--- src/pages/Uslugi.js
+++ src/pages/Uslugi.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Collapsible from 'react-collapsible';
+import Project1 from '../componets/Project1'
+import Project2 from '../componets/Project2'
+import Project3 from '../componets/Project3'
 
-export default function Uslugi() {
-  return (
-    <div className='lol'>
-      <h1>Услуги</h1>
-      <ul>
-        <li>подготовка заданий на проектирование;</li>
-        <li>подготовка и сбор исходных данных для проектирования;</li>
-        <li>оформление исходно-разрешительной документации;
-</li>
-        <li>подготовка конкурсной документации для проведения конкурентных процедур и закупок (тендеров), расчет стартовых цен;
-</li>
-        <li>разработка СТУ</li>
-        <li>комплектация строек оборудованием и материалами;</li>
-      </ul>
-    </div>
-  )
+export default class Uslugi extends Component {
+
+  render() {
+    return (
+      <div className='uslugi'> 
+        <h1>Услуги</h1>
+        <div className='projectsList'>
+          <Collapsible trigger='Проектные работы'> <Project1/> </Collapsible> 
+          <Collapsible trigger='Комплексные инженерные изыскания'> <Project2/> </Collapsible>
+          <Collapsible trigger='Инжиниринговые услуги'> <Project3/> </Collapsible>
+        </div>
+      </div>
+    )
+  }
 }
