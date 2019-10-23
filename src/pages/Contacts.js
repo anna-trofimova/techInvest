0e68de33c19ed19273a1ad41cd19d330881254db
Anna Trogimova <cobrana1000@gmail.com>
2019-10-23 18:37:18 +0300

    contacts css

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -2,22 +2,20 @@ import React from 'react'
 
 export default function Contacts() {
   return (
-    <div>
+    <div className='contacts'>
       <h1>Контакты</h1>
       <article>
-        <h5>Адрес:</h5>
+        <h4>Адрес:</h4>
         <address>
           <p>г. Москва </p>
           <p>107014, Большая Остроумовская, д.12</p>
         </address>
       </article>
       <article>
-        <h5>Телефон:</h5>
+        <h4>Телефон:</h4>
         <p>+7(495) 585-07-59</p>
         <p>info@veglas-e.ru</p>
       </article>
-
-
     </div>
   )
 }
