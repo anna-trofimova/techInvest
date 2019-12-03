f180f366326db28ada75b02be09c105640da0928
Anna Trogimova <cobrana1000@gmail.com>
2019-12-03 18:36:48 +0300

    css for uslugi page

--- src/pages/Uslugi.js
+++ src/pages/Uslugi.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Collapsible from 'react-collapsible';
+// import Collapsible from 'react-collapsible';
 import Project1 from '../componets/Project1'
 import Project2 from '../componets/Project2'
 import Project3 from '../componets/Project3'
@@ -11,9 +11,12 @@ export default class Uslugi extends Component {
       <div className='uslugi'> 
         <h1>Услуги</h1>
         <div className='projectsList'>
-          <Collapsible className='line1' trigger='Проектные работы'> <Project1/> </Collapsible> 
+          {/* <Collapsible className='line1' trigger='Проектные работы'> <Project1/> </Collapsible> 
           <Collapsible className='line2' trigger='Комплексные инженерные изыскания'> <Project2/> </Collapsible>
-          <Collapsible className='line3' trigger='Инжиниринговые услуги'> <Project3/> </Collapsible>
+          <Collapsible className='line3' trigger='Инжиниринговые услуги'> <Project3/> </Collapsible> */}
+          <Project1/>
+          <Project2/>
+          <Project3/>
         </div>
       </div>
     )
